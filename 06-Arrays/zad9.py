4970464461d5fcbb6d9bd136e1a258a5401b8688
Tymon <tymo4444@wp.pl>
2022-11-13 22:08:00 +0100

    13.11.22 - kilka zadań

---
+++ 06-Arrays/zad9.py
@@ -0,0 +1,26 @@
+arr = ['Genowefa',  'Onufry', 'Celestyna', 'Alojzy', 'Pankracy']
+
+
+setcounetr = []
+for i in arr:
+        counter = len(i)
+        setcounetr.append(counter)
+        
+
+maincounter = max(setcounetr)
+print(maincounter)
+        
+
+# 8
+#len(str(i.count(arr[0])))
+
+"""
+setcounetr = []
+for i in arr:
+        counter = len(i)
+        setcounetr.append(counter)
+        
+
+maincounter = max(setcounetr)
+print(maincounter)
+"""
